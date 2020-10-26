c576c18b4d7c6dc18b85318e507dd8f868c18178
Phil Mass <philrmass@gmail.com>
2020-10-26 14:58:02 -0700

    inc version

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/index.css';
 import store from './redux/store';
 import App from './components/App';
 
-const version = '0.8.2';
+const version = '0.8.3';
 
 ReactDOM.render(
   <Provider store={store}>
