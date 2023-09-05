1c09d0a37cbb256a6fe603dbcb095d77fa82fdd8
Phil Mass <philrmass@gmail.com>
2023-09-04 20:02:17 -0700

    cleanup

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,21 +8,15 @@ import Redirect from './Redirect';
 // import Weekly from './Weekly';
 
 export default function Routes() {
-
   return (
-    <main>
-      <Router
-        history={createHashHistory()}
-        onChange={(e) => console.log('URL-CHANGE', e)}
-      >
-        <Home path="/" />
-        <Redirect default to="/" />
-        {/*
+    <Router history={createHashHistory()}>
+      <Home path="/" />
+      <Redirect default to="/" />
+      {/*
         <Route path='/weight-tracker/weekly'><Weekly /></Route>
         <Route path='/weight-tracker/monthly'><Monthly /></Route>
         <Route path='/weight-tracker/graph'><GraphPage /></Route>
-      */}
-      </Router>
-    </main>
+        */}
+    </Router>
   );
 }
